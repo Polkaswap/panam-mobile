8dbb0223dcf8bb016a688b145498523fc1f23041
AmiralYogi <carthymariejoseph@gmail.com>
2020-12-02 00:25:46 +0100

    WIP
    welcome-screen

--- App.js
+++ App.js
@@ -1,7 +1,47 @@
 import React from 'react';
-import { Provider as ProviderPeper } from 'react-native-paper';
-import Theme from './core/Theme';
+import { configureFonts, DefaultTheme, Provider as ProviderPeper } from 'react-native-paper';
+
 import App from './index.js';
+
+const fontConfig = {
+  regular: {
+    fontFamily: 'Roboto',
+    fontWeight: '400',
+  },
+  medium: {
+    fontFamily: 'Roboto',
+    fontWeight: '500',
+  },
+  light: {
+    fontFamily: 'Roboto',
+    fontWeight: '300',
+  },
+  thin: {
+    fontFamily: 'Roboto',
+    fontWeight: '100',
+  },
+}
+
+const theme = {
+  dark: true,
+  roundness: 4,
+  colors: {
+    primary: '#ffb820',
+    accent: '#03dac4',
+    background: '#f6f6f6',
+    surface: DefaultTheme.colors.surface,
+    error: '#B00020',
+    text: DefaultTheme.colors.text,
+    onBackground: '#000000',
+    onSurface: '#000000',
+    disabled: DefaultTheme.colors.disabled,
+    placeholder: DefaultTheme.colors.placeholder,
+    backdrop: DefaultTheme.colors.backdrop,
+    notification: DefaultTheme.colors.notification,
+  },
+  animation: { scale: 1.0 },
+  fonts: configureFonts(fontConfig),
+};
 
 //const { store, persistor } = configureStore();
 
@@ -9,7 +49,7 @@ export default function Main() {
   return (
     //<Provider /*store={store}*/>
       //<PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-        <ProviderPeper theme={Theme}>
+        <ProviderPeper theme={theme}>
           <App />
         </ProviderPeper>
       //</PersistGate>
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,17 @@
 import React, { memo } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PanamButton from '../component/button';
 
 const WelcomeScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
-            <Text>Welcome Screen</Text>
+            <Text style={styles.title}>Panam</Text>
             <StatusBar style="auto" />
+            <View>
+                <PanamButton icon="account-circle" message="Login" mode="contained" />
+                <PanamButton icon="arrow-right-thick" message="Create your account" mode="contained" />
+            </View>
         </View>
     )
 };
@@ -14,10 +19,13 @@ const WelcomeScreen = ({navigation}) => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#c7ecee',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: "space-around",
     },
-  });
+    title: {
+        fontSize: 50,
+    }
+});
 
-  export default memo(WelcomeScreen);
+export default memo(WelcomeScreen);
